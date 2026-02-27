ef9ed34bae249eb0a1a07bb8ec065bd5e732c68e
Guruprasaath D <gurukprs@gmail.com>
2026-02-27 09:26:23 +0530

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  # Raises error for missing translations and annotates rendered view with file names.
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
